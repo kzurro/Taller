aca3617e0a87e2421836794482bb8bb61896b390
kzurro <pistolo@pistolo.com>
2022-11-07 14:22:08 +0100

    Interface reparable

---
+++ src/es/mde/entidades/Reparable.java
@@ -0,0 +1,37 @@
+package es.mde.entidades;
+
+import java.util.Collection;
+
+
+public interface Reparable {
+	
+	Collection<Averia> getAverias();
+	  
+	  default void addAveria(Averia averia) {
+	    getAverias().add(averia);
+	  }
+	  
+	  default boolean isReparado() {
+	    return getAverias().isEmpty();
+	  }
+	  
+	  default float getHorasReparacion() {
+	    float tiempoTotal = 0;
+	    for (Averia averia : getAverias()) {
+	      tiempoTotal += averia.getNumeroHorasManoObra();
+	    }
+	    
+	    return tiempoTotal;
+	  }
+	  
+	  default float getCosteReparacion() {
+	    float tiempoTotal = 0;
+	    for (Averia averia : getAverias()) {
+	      tiempoTotal += averia.getPrecio();
+	    }
+	    
+	    return tiempoTotal;
+	  }
+	  
+
+}
